fa68fccfb0b52cfdf47a5aa6541ab067ee2fdb6c
manishsolanki1703 <156704520+manishsolanki1703@users.noreply.github.com>
2026-07-28 19:34:32 +0530

    Time: 971 ms (17.72%) | Memory: 517.5 MB (7.90%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -1,15 +1,67 @@
 class Solution {
+    vector<int> ans;
+    void MergeSort(vector<pair<int,int>>& arr , int l , int r)
+    {
+        if (l>=r) return;
+
+        int mid = (l+r) >> 1;
+        MergeSort(arr , l , mid);
+        MergeSort(arr , mid+1 , r);
+
+        int i = l , j = mid+1 , RightCounted = 0;
+        vector<pair<int,int>> temp;
+        while(i<=mid && j<=r)
+        {
+            if(arr[j].first < arr[i].first)
+            {
+                temp.push_back(arr[j++]);
+                RightCounted++;
+            }
+            else
+            {
+                temp.push_back(arr[i]);
+                ans[arr[i++].second] += RightCounted;
+            }
+        }
+
+        while (i<=mid)
+        {
+            temp.push_back(arr[i]);
+            ans[arr[i++].second] += RightCounted;
+        }
+
+        while (j<=r)
+        {
+            temp.push_back(arr[j++]);
+        }
+
+        for(int k=l ; k<=r ; k++)
+        {
+            arr[k] = temp[k-l];
+        }
+    }
+
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        int n =  nums.size();
-        vector<int> sorted_arr ,ans(n);
+        // int n =  nums.size();
+        // vector<int> sorted_arr ,ans(n);
 
-        for(int i=n-1 ;i>=0 ; i--)
-        {
-            auto it = lower_bound(sorted_arr.begin(),sorted_arr.end(),nums[i]);
-            ans[i] = it - sorted_arr.begin();
-            sorted_arr.insert(it,nums[i]);
-        }
+        // for(int i=n-1 ;i>=0 ; i--)
+        // {
+        //     auto it = lower_bound(sorted_arr.begin(),sorted_arr.end(),nums[i]);
+        //     ans[i] = it - sorted_arr.begin();
+        //     sorted_arr.insert(it,nums[i]);
+        // }
+
+        // return ans;
+
+        int n = nums.size();
+        vector<pair<int,int>> arr;
+        ans.assign(n,0);
+
+        for(int i=0 ; i<n ; i++) arr.push_back({nums[i],i});
+
+        MergeSort(arr,0,n-1);
 
         return ans;
     }
