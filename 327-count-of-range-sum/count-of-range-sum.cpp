1949b39af6721deaba9d2102572a386177d6c030
manishsolanki1703 <156704520+manishsolanki1703@users.noreply.github.com>
2026-07-29 10:34:35 +0530

    Time: 237 ms (85.20%) | Memory: 123.6 MB (80.39%) - LeetSync

--- 327-count-of-range-sum/count-of-range-sum.cpp
+++ 327-count-of-range-sum/count-of-range-sum.cpp
@@ -31,15 +31,75 @@ class Solution {
         for(int k=l ; k<r ;k++) arr[k] = temp[k-l];
         return count;
     }
+
+    class Fenwick {
+        vector<int> bit;
+        int n;
+    
+    public :
+        Fenwick (int n){
+            this->n = n;
+            bit.assign(n+1,0);
+        }
+
+        void update(int idx , int val)
+        {
+            while (idx<=n)
+            {
+                bit[idx]+=val;
+                idx += idx & (-idx);
+            }
+        }
+
+        int query(int idx)
+        {
+            int sum = 0;
+            while (idx>0)
+            {
+                sum += bit[idx];
+                idx-= idx & (-idx);
+            }
+            return sum;
+        }
+    };
+
 public:
     int countRangeSum(vector<int>& nums, int lower, int upper) {
-        this->lower = lower;
-        this->upper = upper;
+        // this->lower = lower;
+        // this->upper = upper;
 
         int n = nums.size();
         vector<long long> prefix_sum(n+1,0);
         for(int i=0 ; i<n ; i++) prefix_sum[i+1] = prefix_sum[i] + nums[i];
 
-        return MergeSort(prefix_sum , 0 , n+1); 
+        // return MergeSort(prefix_sum , 0 , n+1); 
+
+        vector<long long> arr;
+        for(auto it : prefix_sum)
+        {
+            arr.push_back(it); 
+            arr.push_back(it - lower); 
+            arr.push_back(it-upper);
+        }
+        
+        sort(arr.begin(),arr.end());
+        arr.erase(unique(arr.begin(),arr.end()) , arr.end());
+        Fenwick ft(arr.size());
+
+        long long ans = 0;
+
+        for(auto it : prefix_sum)
+        {
+            int left = lower_bound(arr.begin(),arr.end(),it - upper) - arr.begin() + 1;
+            int right = upper_bound(arr.begin(),arr.end(),it - lower) - arr.begin();
+
+            ans += ft.query(right) - ft.query(left-1);
+
+            int idx = lower_bound(arr.begin(),arr.end(),it) - arr.begin() + 1;
+
+            ft.update(idx , 1);
+        }
+
+        return ans;
     }
 };
