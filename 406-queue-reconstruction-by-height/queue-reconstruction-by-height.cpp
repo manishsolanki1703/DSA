32f1a31db914396e8575816f6e4c3498bace0f86
manishsolanki1703 <156704520+manishsolanki1703@users.noreply.github.com>
2026-07-29 15:14:24 +0530

    Time: 16 ms (72.92%) | Memory: 15.9 MB (78.49%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        vector<vector<int>> temp = people;
 
-        sort(temp.begin(),temp.end(), [](const vector<int>& a, const vector<int>& b){
-            if(a[0] == b[0]) return a[1] > b[1];
-            return a[0] < b[0];
-        });
+        sort(people.begin(), people.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] < b[1];
+                 return a[0] > b[0];
+             });
 
-        vector<vector<int>> ans(people.size(),vector<int>(2,-1));
-        
-        for(auto it : temp)
-        {
-            int count = it[1];
-            for(int i=0 ; i<ans.size() ; i++)
-            {
-                if (count == 0 && ans[i][0] == -1) 
-                {
-                    ans[i] = it;
-                    break;
-                }
-                if (ans[i][0] == -1) count--;
-            }
-        }
+        vector<vector<int>> ans;
+
+        for (auto &p : people)
+            ans.insert(ans.begin() + p[1], p);
 
         return ans;
     }
